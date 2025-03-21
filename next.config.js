4c6d2194f38d4a616252af0e8f91218801b0e3dc
Jay Namburi <nambjay@gmail.com>
2025-03-21 17:26:30 -0500

    Update Next.js config and dependencies for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
     unoptimized: true,
+    domains: ['localhost'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
       },
     ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(svg|png|jpg|jpeg|gif)$/,
+      type: 'asset/resource',
+    })
+    return config
   },
   typescript: {
     ignoreBuildErrors: true,
